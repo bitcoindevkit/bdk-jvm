e3516fe09e389cc7aa0303c685a6ef6dd756b8e4
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-08-28 11:29:58 -0400

    test: add constants file

---
+++ lib/src/test/kotlin/org/bitcoindevkit/Constants.kt
@@ -0,0 +1,26 @@
+package org.bitcoindevkit
+
+val BIP84_DESCRIPTOR: Descriptor = Descriptor(
+    "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/0/*)",
+    Network.TESTNET
+)
+val BIP84_CHANGE_DESCRIPTOR: Descriptor = Descriptor(
+    "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/1/*)",
+    Network.TESTNET
+)
+val BIP86_DESCRIPTOR: Descriptor = Descriptor(
+    "tr(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/86h/1h/1h/0/*)",
+    Network.TESTNET
+)
+val BIP86_CHANGE_DESCRIPTOR: Descriptor = Descriptor(
+    "tr(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/86h/1h/1h/1/*)",
+    Network.TESTNET
+)
+val NON_EXTENDED_DESCRIPTOR_0: Descriptor = Descriptor(
+    descriptor = "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/0/0)",
+    network = Network.TESTNET
+)
+val NON_EXTENDED_DESCRIPTOR_1: Descriptor = Descriptor(
+    descriptor = "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/0/1)",
+    network = Network.TESTNET
+)
