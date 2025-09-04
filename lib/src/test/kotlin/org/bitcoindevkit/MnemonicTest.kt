334710c4b9c6a6e02f2c13bfec93f3baacec14f9
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-09-04 11:44:34 -0400

    test: add new streamlined constants

--- lib/src/test/kotlin/org/bitcoindevkit/MnemonicTest.kt
+++ lib/src/test/kotlin/org/bitcoindevkit/MnemonicTest.kt
@@ -6,12 +6,12 @@ import kotlin.test.assertEquals
 class MnemonicTest {
     @Test
     fun `Mnemonics create valid descriptors`() {
-        val mnemonic: Mnemonic = Mnemonic.fromString("space echo position wrist orient erupt relief museum myself grain wisdom tumble")
-        val descriptorSecretKey: DescriptorSecretKey = DescriptorSecretKey(Network.TESTNET, mnemonic, null)
-        val descriptor: Descriptor = Descriptor.newBip86(descriptorSecretKey, KeychainKind.EXTERNAL, Network.TESTNET)
+        val mnemonic: Mnemonic = Mnemonic.fromString(MNEMONIC_AWESOME)
+        val descriptorSecretKey: DescriptorSecretKey = DescriptorSecretKey(Network.TESTNET4, mnemonic, null)
+        val descriptor: Descriptor = Descriptor.newBip86(descriptorSecretKey, KeychainKind.EXTERNAL, Network.TESTNET4)
 
         assertEquals(
-            expected = "tr([be1eec8f/86'/1'/0']tpubDCTtszwSxPx3tATqDrsSyqScPNnUChwQAVAkanuDUCJQESGBbkt68nXXKRDifYSDbeMa2Xg2euKbXaU3YphvGWftDE7ozRKPriT6vAo3xsc/0/*)#m7puekcx",
+            expected = "tr([5bc5d243/86'/1'/0']tpubDC72NVP1RK5qwy2QdEfWphDsUBAfBu7oiV6jEFooHP8tGQGFVUeFxhgZxuk1j6EQRJ1YsS3th2RyDgReRqCL4zqp4jtuV2z7gbiqDH2iyUS/0/*)#xh44xwsp",
             actual = descriptor.toString()
         )
     }
