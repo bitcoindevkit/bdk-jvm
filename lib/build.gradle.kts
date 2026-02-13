d68fb91d661c25ca56745be956822b3d98636bee
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-02-13 11:09:32 -0500

    build: use com.vanniktech.maven.publish plugin for publication

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -5,8 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
-    id("org.gradle.maven-publish")
-    id("org.gradle.signing")
+    id("com.vanniktech.maven.publish")
     id("org.jetbrains.dokka")
     id("org.jetbrains.dokka-javadoc")
 }
@@ -17,8 +16,6 @@ version = "2.3.0-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-    withSourcesJar()
-    withJavadocJar()
 }
 
 tasks.withType<KotlinCompile> {
@@ -50,52 +47,46 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = "org.bitcoindevkit"
-                artifactId = "bdk-jvm"
-                version = version.toString()
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "bdk-jvm",
+        version = version.toString()
+    )
 
-                from(components["java"])
-                pom {
-                    name.set("bdk-jvm")
-                    description.set("Bitcoin Dev Kit Kotlin language bindings.")
-                    url.set("https://bitcoindevkit.org")
-                    licenses {
-                        license {
-                            name.set("APACHE 2.0")
-                            url.set("https://github.com/bitcoindevkit/bdk/blob/master/LICENSE-APACHE")
-                        }
-                        license {
-                            name.set("MIT")
-                            url.set("https://github.com/bitcoindevkit/bdk/blob/master/LICENSE-MIT")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("bdkdevelopers")
-                            name.set("Bitcoin Dev Kit Developers")
-                            email.set("dev@bitcoindevkit.org")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:github.com/bitcoindevkit/bdk-ffi.git")
-                        developerConnection.set("scm:git:ssh://github.com/bitcoindevkit/bdk-ffi.git")
-                        url.set("https://github.com/bitcoindevkit/bdk-ffi/tree/master")
-                    }
-                }
+    pom {
+        name.set("bdk-jvm")
+        description.set("Bitcoin Dev Kit Kotlin language bindings.")
+        url.set("https://bitcoindevkit.org")
+        inceptionYear.set("2021")
+        licenses {
+            license {
+                name.set("APACHE 2.0")
+                url.set("https://github.com/bitcoindevkit/bdk/blob/master/LICENSE-APACHE")
             }
+            license {
+                name.set("MIT")
+                url.set("https://github.com/bitcoindevkit/bdk/blob/master/LICENSE-MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("bdkdevelopers")
+                name.set("Bitcoin Dev Kit Developers")
+                email.set("dev@bitcoindevkit.org")
+            }
+        }
+        scm {
+            url.set("https://github.com/bitcoindevkit/bdk-ffi/")
+            connection.set("scm:git:github.com/bitcoindevkit/bdk-ffi.git")
+            developerConnection.set("scm:git:ssh://github.com/bitcoindevkit/bdk-ffi.git")
         }
     }
 }
 
-signing {
-    if (project.hasProperty("localBuild")) {
-        isRequired = false
-    }
-    sign(publishing.publications)
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 }
 
 dokka {
