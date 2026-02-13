049bbdf97a754492fc9bddb95855dc12cd317428
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-02-13 11:09:32 -0500

    build: use com.vanniktech.maven.publish plugin for publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("2.1.10").apply(false)
-    id("org.gradle.maven-publish")
-    id("org.gradle.signing")
+    id("org.jetbrains.kotlin.jvm").version("2.2.0").apply(false)
     id("org.jetbrains.dokka").version("2.0.0").apply(false)
     id("org.jetbrains.dokka-javadoc").version("2.0.0").apply(false)
+    id("com.vanniktech.maven.publish").version("0.36.0").apply(false)
 }
