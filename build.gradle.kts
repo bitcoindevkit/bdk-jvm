58176a0a7ccab42f603aaef28623e666e3765425
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-02-19 12:55:09 -0500

    docs: provide Dokka docs website in the javadoc.jar artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.jetbrains.kotlin.jvm").version("2.2.0").apply(false)
-    id("org.jetbrains.dokka").version("2.0.0").apply(false)
-    id("org.jetbrains.dokka-javadoc").version("2.0.0").apply(false)
+    id("org.jetbrains.dokka").version("2.1.0").apply(false)
     id("com.vanniktech.maven.publish").version("0.36.0").apply(false)
 }
