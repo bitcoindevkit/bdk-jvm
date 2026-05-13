73cb0831e1ef0146644f8284956580ac12b07356
itorod <itoroukpong1@gmail.com>
2026-05-13 11:37:22 +0000

    refactor: Remove checks for NetworkKind.TEST

--- lib/src/test/kotlin/org/bitcoindevkit/DescriptorTest.kt
+++ lib/src/test/kotlin/org/bitcoindevkit/DescriptorTest.kt
@@ -10,17 +10,11 @@ class DescriptorTest {
         @Test
         fun `Create extended WPKH descriptors for all networks`() {
             Descriptor("wpkh($TEST_EXTENDED_PRIVKEY/$BIP84_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
-            Descriptor("wpkh($TEST_EXTENDED_PRIVKEY/$BIP84_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
-            Descriptor("wpkh($TEST_EXTENDED_PRIVKEY/$BIP84_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
-            Descriptor("wpkh($TEST_EXTENDED_PRIVKEY/$BIP84_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
             Descriptor("wpkh($MAIN_EXTENDED_PRIVKEY/$BIP84_MAIN_RECEIVE_PATH/*)", NetworkKind.MAIN)
         }
 
         @Test
         fun `Create extended TR descriptors for all networks`() {
-            Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
             Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/*)", NetworkKind.TEST)
             Descriptor("tr($MAIN_EXTENDED_PRIVKEY/$BIP86_MAIN_RECEIVE_PATH/*)", NetworkKind.MAIN)
         }
@@ -28,17 +22,11 @@ class DescriptorTest {
         @Test
         fun `Create non-extended descriptors for all networks`() {
             Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/0)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/0)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/0)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PRIVKEY/$BIP86_TEST_RECEIVE_PATH/0)", NetworkKind.TEST)
             Descriptor("tr($MAIN_EXTENDED_PRIVKEY/$BIP86_MAIN_RECEIVE_PATH/0)", NetworkKind.MAIN)
         }
 
         @Test
         fun `Create descriptors from multipath public descriptor strings`() {
-            Descriptor("tr($TEST_EXTENDED_PUBKEY/<0;1>/*)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PUBKEY/<0;1>/*)", NetworkKind.TEST)
-            Descriptor("tr($TEST_EXTENDED_PUBKEY/<0;1>/*)", NetworkKind.TEST)
             Descriptor("tr($TEST_EXTENDED_PUBKEY/<0;1>/*)", NetworkKind.TEST)
             Descriptor("tr($MAIN_EXTENDED_PUBKEY/<0;1>/*)", NetworkKind.MAIN)
         }
