4cb0baf3cc0e85e934a227d2cf549cb4c91beb5a
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-05-25 14:17:14 -0400

    build: small cleanups of our build scripts

--- scripts/build-linux-x86_64.sh
+++ scripts/build-linux-x86_64.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 COMPILATION_TARGET="x86_64-unknown-linux-gnu"
 TARGET_DIR="target/x86_64-unknown-linux-gnu/release-smaller"
@@ -6,7 +7,7 @@ RESOURCE_DIR="resources/linux-x86-64"
 LIB_NAME="libbdkffi.so"
 
 printf "\nSubmodule check...\n"
-if [[ "$1" != "--skip-submodule-update" ]]; then
+if [[ "${1:-}" != "--skip-submodule-update" ]]; then
   git submodule update --init
   printf "Submodule is checked out at commit: $(git submodule status)\n\n"
 else
@@ -19,7 +20,7 @@ cd ./bdk-ffi/bdk-ffi/ || exit
 
 # Build the Rust library
 rustup target add $COMPILATION_TARGET
-cargo build --profile release-smaller --target $COMPILATION_TARGET
+cargo build --locked --profile release-smaller --target $COMPILATION_TARGET
 
 # Generate Kotlin bindings using uniffi-bindgen
 cargo run --bin uniffi-bindgen generate --library ./$TARGET_DIR/$LIB_NAME --language kotlin --out-dir ../../lib/src/main/kotlin/ --no-format
